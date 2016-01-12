d03e0c9f68fa6b9530d9aac0805da1635818a26b
emxxxm <emxxxm@gmail.com>
2016-01-12 10:01:23 -0800

    update PMECommitmentSubroutine.java PME DeliveryData.java

--- HellloJetty/src/atfImplementation/PMECommitment/PMECommitmentSubroutine.java
+++ HellloJetty/src/atfImplementation/PMECommitment/PMECommitmentSubroutine.java
@@ -2,5 +2,31 @@ package atfImplementation.PMECommitment;
 
 public class PMECommitmentSubroutine {
 	
+	//[DataAccess] Lookup Origin ZIP in [ATF_PME_ORIGIN_SCHEDULE]
+	//save all Records
+	
+	//[DataAccess] Lookup 5-Digit and 3-Digit Dest ZIP for chosen Dest Type in 
+	//[ATF_PME_DEST_SCHEDULE; Save all Records
+	
+	//While(Origin Records remaining && NOT Destination Records remaining){
+		//Select 1 Origin Record to create a COmmitment with
+		
+	//if(NOT Origin Records remaining){
+		//if(NOT any commitments exists){
+			//if(Drop Off Time <= DOW COT from AFT_COT){
+				// Service Standard = 2;
+				// Delivery Time = 1500;
+				// Commitment Date = EAD;
+				// Commitment Rank = 1;
+				// Preferred Indicator = 0;}
+			//else{
+				//Service Standard = 2;
+				//Delivery Time = 1500;
+				//Commitment Date = EAD + 1;
+				//cOMMITMENT rANK = 1;
+				//Preferred Indicator = 0}}}
+	
+		
+	
 
 }
--- HellloJetty/src/atfImplementation/PMECommitment/PMEDeliveryDate.java
+++ HellloJetty/src/atfImplementation/PMECommitment/PMEDeliveryDate.java
@@ -1,5 +1,42 @@
 package atfImplementation.PMECommitment;
 
 public class PMEDeliveryDate {
-
+	//[Drools] Execute Rules Engine for Delivery Date Rules
+	
+	//if(Delivery Date > 7 Days after the EAD){
+		//Set Delivery Date = EAD + 7 Days
+		//return;}
+	
+	//else{
+		//Set DOW = Delivery date DOW
+		//if(Destination == HFPU){
+			//if(Delivery Date is a holiday)
+				//Set DOW = 8
+			//[DataAccess] Lookup Dow Close Time from [ATF_ADDRESS_CLOSE]
+			//!!!TODO REDESGIN  if CLOSE TIME = 0 , Go back to line 4} 
+		//else{
+			//if(DOW==Saturday or Sunday)
+				//if(User select "No Delivery" on this DOW){
+					//Delivery Date = Delivery Date + 1
+					//Go back to line 4!!!!!!}
+			//if(Delivery Date is a holiday){
+				//if(User select "No Delivery" on Holidays)
+					//Set Delivery date = Delivery Date + 1}
+			//else DOW = 8}
+	
+		//if Destination PO Box
+			//Lookup DOW Close Time from ATF_ADDRESS_CLOSE
+			//If CLose Time = 0
+				//Set Delivery Date = Delivery Date + 1
+				//GO back to line 4}
+	//If Retrograde ZIP is set 
+		//Orizin ZIP = Retrograde ZIP
+	
+	//else if(Prograde ZIP set)
+		//Destination ZIP = Prograde ZIP
+	
+	//return;
+	
+	
+			
 }
