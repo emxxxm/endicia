0cdc96bd52daeea41de27f050f59a06a9d630a59
unknown <jcanas@ENGLOANER3-WLT.psisystems.local>
2016-01-11 16:13:52 -0800

    Finished APO/FPO/DPO subroutine. Added PsuedoCode to bestPMEcommitment

--- HellloJetty/src/atfImplementation/PMECommitment/APO_FPO_DPO_Subroutine.java
+++ HellloJetty/src/atfImplementation/PMECommitment/APO_FPO_DPO_Subroutine.java
@@ -7,10 +7,10 @@ import atfImplementation.CalculationNotPossibleException;
 
 public class APO_FPO_DPO_Subroutine {
 	
-	int progradeOffset=0,retrogradeOffset=0, retrogradeOffsetFromRecord=-1, postgradeOffsetFromRecord;
+	int progradeOffset=0,retrogradeOffset=0, retrogradeOffsetFromRecord, progradeOffsetFromRecord;
 	int upperBound=10000,lowerBound=20000;
 	String originZip="01609", destZip ="90610"; //TODO actually get these from the data
-	String retrogradeZip, progradeZip, dropOffTime;
+	String retrogradeZip, progradeZip, dropOffTime, retrogradeArrivalTime;
 	
 	
 	public APO_FPO_DPO_Subroutine(LinkedHashMap<String,String> queryTuples) throws CalculationNotPossibleException {
@@ -20,15 +20,12 @@ public class APO_FPO_DPO_Subroutine {
 		initializeValuesFromDataFile();
 		originZip = queryTuples.get("originZip");
 		destZip = queryTuples.get("destZip");
-		
-		int intOriginZip = Integer.parseInt(originZip), intDestZip=Integer.parseInt(destZip);
 	
 		//TODO [DataAccess] lookup Military ZIP ranges in ATF_REF_VALUE
-		if (intOriginZip > lowerBound && intOriginZip < upperBound) {
-			
-			if (intDestZip > lowerBound && intDestZip < upperBound) {
+		if (zipInRange(originZip)) {//Path if origin zip is within military range
+			if (zipInRange(destZip)) {
 				if (originOrDestDPO()) {
-					throw new CalculationNotPossibleException("Either the origin or destination is DPO; calculation not possible");
+					throw new CalculationNotPossibleException("Either the origin or destination ZIP is DPO. Calculation not possible with supplied data");
 		        } else {				
 				return;//progradeOffset and retrogradeOffset are 0
 		        }
@@ -40,13 +37,44 @@ public class APO_FPO_DPO_Subroutine {
 				queryTuples.put("originZip", retrogradeZip);
 				retrogradeZip = originZip;
 				return;
+			} else {
+				dropOffTime = retrogradeArrivalTime;
+				throw new CalculationNotPossibleException("Failed to find a record during lookup with supplied Origin ZIP and Mail Class. Calculation not possible with supplied data");
 			}
 		
-		}
+		} else {//Path if origin zip is not within military range
+			if (zipInRange(destZip)) {
+				//[DataAccess] look up origin zip and mail class from ATF_APOFPODPO
+				if (dataFromFile.size() == 1) {//If one record is found
+					if (isHFPU()) {
+						throw new CalculationNotPossibleException("Destination Type is either PO Box or HFPU. Calculation is not possible with the supplied data.");
+					} else {
+						progradeOffset = progradeOffsetFromRecord;
+						queryTuples.put("destZip", progradeZip);
+						progradeZip = destZip;
+					}
+				}
+			} else {
+				return;
+			}
+		}	
 	}
 	
+	private boolean isHFPU() {
+		return false;
+	}
+
 	private void initializeValuesFromDataFile() {
 		//TODO [DataAccess]
+		dropOffTime = "-1";
+		retrogradeArrivalTime= "-1";
+		retrogradeOffsetFromRecord=-1;
+		progradeOffsetFromRecord=-1;
+	}
+	
+	private boolean zipInRange(String zip) {
+		int intZip = Integer.parseInt(zip);
+		return intZip > lowerBound && intZip < upperBound;
 	}
 			
 	private boolean originOrDestDPO() {
--- HellloJetty/src/atfImplementation/PMECommitment/BestPMECommitment.java
+++ HellloJetty/src/atfImplementation/PMECommitment/BestPMECommitment.java
@@ -1,5 +1,21 @@
 package atfImplementation.PMECommitment;
 
 public class BestPMECommitment {
-
+	
+	//Get commitments from the PMECommitmentSubroutine
+	
+	//Sort Commitments by Preliminary Date in descending order (earliest date first).
+	//Keep only the commitments that share the earliest preliminary date
+	
+	//Sort commitments by Commitment Rank in descending order
+	//Keep only commitments with highest Commitment Rank
+	
+	//if (any commitments have preferred indicator == -1) 
+		//Keep only commitments with preferred indicator == 1 
+    
+	//Save the first commitment where deliveryTime = Earliest Delivery Time
+	
+	//transitTime = serviceStandard
+	//EAD = commitmentDate
+	
 }
