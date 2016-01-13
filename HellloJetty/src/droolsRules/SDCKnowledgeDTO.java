b5c4151ccfa82db00606f96e8e9af86324b0d065
Nicholas Muesch <nmuesch@psisystems.local>
2016-01-13 14:56:01 -0800

    Adds Drools Rules

---
+++ HellloJetty/src/droolsRules/SDCKnowledgeDTO.java
@@ -0,0 +1,12 @@
+package droolsRules;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+
+public class SDCKnowledgeDTO {
+	public void incrementEad() {
+		
+	}
+}
