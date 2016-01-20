cb5c6f63a54eb1f7b4f724afd316ccf4385f5770
unknown <jcanas@ENGLOANER3-WLT.psisystems.local>
2016-01-20 11:14:13 -0800

    Progress on main logic, deleted old main logic classes in different pkg

--- HellloJetty/src/atfImplementation/MainATFImplementation.java
+++ HellloJetty/src/atfImplementation/MainATFImplementation.java
@@ -2,11 +2,8 @@ package atfImplementation;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import MainPackage.QueryStrings;
-import atfImplementation.nonPMECommitment.NonPMECommitmentMainLogic;
 
 public class MainATFImplementation  {
 	
--- HellloJetty/src/atfImplementation/MainNonPMEImplementation.java
+++ HellloJetty/src/atfImplementation/MainNonPMEImplementation.java
@@ -3,6 +3,7 @@ package atfImplementation;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import MainPackage.QueryStrings;
 import atfImplementation.nonPMECommitment.PRI_COT;
 
 public class MainNonPMEImplementation extends AbsATFImplementation {
@@ -16,8 +17,33 @@ public class MainNonPMEImplementation extends AbsATFImplementation {
 
 	@Override
 	public void execute() {
-		ArrayList<String> PRIOutput = pri.getPRI_COT();
+		ArrayList<String> PRIOutput; 
+		
+		if (queryTuples.get(QueryStrings.MAIL_CLASS).equals(QueryStrings.MAIL_CLASS_PRI)) {
+			PRIOutput = pri.getPRI_COT();
+		} else {
+			
+		}
 		
 	}
+	
+	//TODO determine the class type 
+	//if type is is PRI, getPRICOT()
+	//else get_EAD_DOW_COT()
+
+//if isDestHFPU, 
+	//Get HFPULocation
+
+//Lookup Origin Close Times from ATF_ADDRESS_CLOSE
+
+//[DROOLS] Execute Rules Engine for Acceptance Rules
+
+//Get NonPMEServiceStandard
+
+//[DROOLS]Execute Rules Engine for Transit Time Rules
+
+//DeliveryDate = EAD + Transit Time
+
+//create subroutine calculateNonPMEDeliveryDate
 
 }
