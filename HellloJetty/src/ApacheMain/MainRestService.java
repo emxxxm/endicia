a7858b1bdb23edb6ea63bcdcd06c0935fbd0c8a8
Justin Canas <justincanas16@gmail.com>
2016-02-04 11:51:17 -0800

    incoming merge conflicts due to nicks face

--- HellloJetty/src/ApacheMain/MainRestService.java
+++ HellloJetty/src/ApacheMain/MainRestService.java
@@ -29,76 +29,97 @@ import atfImplementation.MainATFImplementation;
 
 //@Path( "/people" ) 
 public class MainRestService {
-    @Inject private OutputService outputService;
-    MainATFImplementation mainLogic;
+	@Inject private OutputService outputService;
+	MainATFImplementation mainLogic;
 
 	private final static Logger logger = Logger.getLogger(LoggingHub.class.getName());
-	
-    
-    @Path("/people")
-    @Produces( { "application/xml"} )
-   // @Consumes( {"application/x-www-form-urlencoded"} )
-    @GET
-    public Collection<DazzleOutputMain> getPeople( @QueryParam( QueryStrings.ORIGIN_ZIP) String originZip, 
-    									   @QueryParam(QueryStrings.DEST_ZIP) String destZip,
-    									   @QueryParam(QueryStrings.SHIP_DATE) @DefaultValue("") String shipDate,
-    									   @QueryParam(QueryStrings.SHIP_TIME) String dropOffTime,
-    									   @QueryParam(QueryStrings.MAIL_CLASS) String mailClass, 
-    									   @QueryParam(QueryStrings.DEST_TYPE) String destType) throws NumberFormatException, CalculationNotPossibleException, ParseException, InvalidQueryFormatException {
-    	Collection<DazzleOutputMain> output = null;
-    	if(shipDate.isEmpty()) {
-    		shipDate = DateTimeUtilities.getCurrentUTCDate();
-    	}
-    	try {
-    		HashMap<String, String> queryTuples = QueryParser.initializeTuples(originZip, destZip, shipDate, dropOffTime, mailClass, destType);
-        	output = outputService.getVerbose(queryTuples);
-    	} catch (Exception e) {
-    		logger.log(Level.SEVERE, e.getMessage(), e);
-    		throw new WebApplicationException(e.getMessage());
-    	}
-    	return output;
-    }
-    
-    @Path("/people.xml")
-    @Produces( {"application/xml"} )
-    @Consumes("")
-    @POST
-    public Collection<Output> getPeopleXMLFromForm() {
-    	
-    	return null;
-    }
-    
-    @Path("/people.xml")
-    @Produces( {"application/xml"} )
-    @Consumes( {"application/x-www-form-urlencoded"} )
-    @POST
-    public Collection<DazzleOutput> getPeopleJSONPost( @Multipart(value = "originzip", type = "text/xml") String originZip) {
-    	System.out.println(originZip);
-    	return null;
-    }
-    
-    @Path("/people.json")
-    @Produces( { "application/json"} )
-    @GET
-    public Collection<DazzleOutputMain> getPeopleJSON(  @QueryParam( QueryStrings.ORIGIN_ZIP) String originZip, 
-			   @QueryParam(QueryStrings.DEST_ZIP) String destZip,
-			   @QueryParam(QueryStrings.SHIP_DATE) @DefaultValue("") String shipDate,
-			   @QueryParam(QueryStrings.SHIP_TIME) String dropOffTime,
-			   @QueryParam(QueryStrings.MAIL_CLASS) String mailClass, 
-			   @QueryParam(QueryStrings.DEST_TYPE) String destType) throws NumberFormatException, CalculationNotPossibleException, ParseException, InvalidQueryFormatException {
-    	if(shipDate.isEmpty()) {
-    		shipDate = DateTimeUtilities.getCurrentUTCDate();
-    	}
-    	HashMap<String, String> queryTuples = QueryParser.initializeTuples(originZip, destZip, shipDate, dropOffTime, mailClass, destType);
-    	Collection<DazzleOutputMain> output = outputService.getVerbose(queryTuples);
-    	return output;
-    }
-    
-    @Path("/people.xml")
-    @Produces( { "application/xml"} )
-    @GET
-    public Collection< Output > getPeopleXML( @QueryParam( "page") @DefaultValue( "1" ) final int page ) {
-        return outputService.getPeople( page, 5 );
-    }
-    
+
+
+	@Path("/mail")
+	@Produces( { "application/xml"} )
+	// @Consumes( {"application/x-www-form-urlencoded"} )
+	@GET
+	public Collection<DazzleOutputMain> getDefaultDazzleOutput( @QueryParam( QueryStrings.ORIGIN_ZIP) String originZip, 
+			@QueryParam(QueryStrings.DEST_ZIP) String destZip,
+			@QueryParam(QueryStrings.SHIP_DATE) @DefaultValue("") String shipDate,
+			@QueryParam(QueryStrings.SHIP_TIME) String dropOffTime,
+			@QueryParam(QueryStrings.MAIL_CLASS) String mailClass, 
+			@QueryParam(QueryStrings.DEST_TYPE) String destType) throws NumberFormatException, CalculationNotPossibleException, ParseException, InvalidQueryFormatException {
+		Collection<DazzleOutputMain> output = null;
+		if(shipDate.isEmpty()) {
+			shipDate = DateTimeUtilities.getCurrentUTCDate();
+		}
+		try {
+			HashMap<String, String> queryTuples = QueryParser.initializeTuples(originZip, destZip, shipDate, dropOffTime, mailClass, destType);
+			output = outputService.getVerbose(queryTuples);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+			throw new WebApplicationException(e.getMessage());
+		}
+		return output;
+	}
+
+	@Path("/mail.xml")
+	@Produces( {"application/xml"} )
+	@Consumes("")
+	@POST
+	public Collection<Output> getPeopleXMLFromForm() {
+		return null;
+	}
+
+	@Path("/mail.xml")
+	@Produces( {"application/xml"} )
+	@Consumes( {"application/x-www-form-urlencoded"} )
+	@POST
+	public Collection<DazzleOutput> getPeopleJSONPost( @Multipart(value = "originzip", type = "text/xml") String originZip) {
+		System.out.println(originZip);
+		return null;
+	}
+
+	@Path("/mail.json")
+	@Produces( { "application/json"} )
+	@GET
+	public Collection<DazzleOutputMain> getPeopleJSON(  @QueryParam( QueryStrings.ORIGIN_ZIP) String originZip, 
+			@QueryParam(QueryStrings.DEST_ZIP) String destZip,
+			@QueryParam(QueryStrings.SHIP_DATE) @DefaultValue("") String shipDate,
+			@QueryParam(QueryStrings.SHIP_TIME) String dropOffTime,
+			@QueryParam(QueryStrings.MAIL_CLASS) String mailClass, 
+			@QueryParam(QueryStrings.DEST_TYPE) String destType) throws NumberFormatException, CalculationNotPossibleException, ParseException, InvalidQueryFormatException {
+		Collection<DazzleOutputMain> output = null;
+		if(shipDate.isEmpty()) {
+			shipDate = DateTimeUtilities.getCurrentUTCDate();
+		}
+		try {
+			HashMap<String, String> queryTuples = QueryParser.initializeTuples(originZip, destZip, shipDate, dropOffTime, mailClass, destType);
+			output = outputService.getVerbose(queryTuples);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+			throw new WebApplicationException(e.getMessage());
+		}
+		return output;
+	}
+
+	@Path("/mail.xml")
+	@Produces( { "application/xml"} )
+	@GET
+	public Collection<DazzleOutputMain> getPeopleXML(  @QueryParam( QueryStrings.ORIGIN_ZIP) String originZip, 
+			@QueryParam(QueryStrings.DEST_ZIP) String destZip,
+			@QueryParam(QueryStrings.SHIP_DATE) @DefaultValue("") String shipDate,
+			@QueryParam(QueryStrings.SHIP_TIME) String dropOffTime,
+			@QueryParam(QueryStrings.MAIL_CLASS) String mailClass, 
+			@QueryParam(QueryStrings.DEST_TYPE) String destType) throws NumberFormatException, CalculationNotPossibleException, ParseException, InvalidQueryFormatException {
+		Collection<DazzleOutputMain> output = null;
+		if(shipDate.isEmpty()) {
+			shipDate = DateTimeUtilities.getCurrentUTCDate();
+		}
+		try {
+			HashMap<String, String> queryTuples = QueryParser.initializeTuples(originZip, destZip, shipDate, dropOffTime, mailClass, destType);
+			output = outputService.getVerbose(queryTuples);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.getMessage(), e);
+			throw new WebApplicationException(e.getMessage());
+		}
+		return output;
+	}
+
 }
