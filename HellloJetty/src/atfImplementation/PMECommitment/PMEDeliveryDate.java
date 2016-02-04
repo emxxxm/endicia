50876ec909d5b71a5cb1d3e05c2617f20ae34bd0
Nicholas Muesch <nmuesch@psisystems.local>
2016-02-04 10:15:27 -0800

    Fixed PME Deliv Date

--- HellloJetty/src/atfImplementation/PMECommitment/PMEDeliveryDate.java
+++ HellloJetty/src/atfImplementation/PMECommitment/PMEDeliveryDate.java
@@ -14,23 +14,23 @@ import droolsRules.SDCKnowledgeDTO;
 
 public class PMEDeliveryDate {
 	//take in initially set in Main Flow, return update Delivery Date
-	
+
 	SDCKnowledgeDTO droolsMsg;
 	int dow;
 	int closeTime=0;
 	String retroZip;
 	String proZip;
 	String deliveryDate;
-	
+
 	public PMEDeliveryDate(HashMap<String, String> queryTuples) throws ParseException, NumberFormatException, CalculationNotPossibleException{
 		droolsMsg = SDCKnowledgeDTO.initializeDroolsMsg(queryTuples, new SDCKnowledgeDTO());
 		while(true){
 			droolsMsg.originZip = queryTuples.get(QueryStrings.ORIGIN_ZIP);
 			droolsMsg.destinationZip = queryTuples.get(QueryStrings.DEST_ZIP);
-	//		droolsMsg.deliveryDate = deliveryDate;
+			//		droolsMsg.deliveryDate = deliveryDate;
 			//[Drools] Execute Rules Engine for Delivery Date Rules 
 			DataMaster.getInstance().getRulesObject().insertAndFire(droolsMsg, RulesObject.DROOLS_DELIVERY);
-			
+
 			if(DateTimeUtilities.getDaysBetweenDates(droolsMsg.deliveryDate, droolsMsg.ead) > 7) {
 				droolsMsg.deliveryDate = DateTimeUtilities.incrementDate(droolsMsg.ead, 7);
 				return;
@@ -47,7 +47,7 @@ public class PMEDeliveryDate {
 						continue;
 					}
 					retroZip = queryTuples.get(QueryStrings.RETROGRADE_ZIP);
-					
+
 					if(retroZip != null) {
 						queryTuples.put(QueryStrings.ORIGIN_ZIP, retroZip);
 					}
@@ -72,93 +72,94 @@ public class PMEDeliveryDate {
 							dow = 8;
 						}
 					}
-					if(queryTuples.get(QueryStrings.DEST_TYPE) == QueryStrings.DESTTYPE_PO_BOX) {
+					if(queryTuples.get(QueryStrings.DEST_TYPE).equals(QueryStrings.DESTTYPE_PO_BOX)) {
 						DataMaster.getInstance().getAddressClose();
 						closeTime = AddressClose.getCloseTimeOnDOWWrapper(dow, queryTuples.get(QueryStrings.DEST_ZIP));
 						if(closeTime == 0){
 							droolsMsg.deliveryDate =  DateTimeUtilities.incrementDate(droolsMsg.deliveryDate, 1);
 							continue;
 						}
-						if(retroZip != null) {
-							queryTuples.put(QueryStrings.ORIGIN_ZIP, retroZip);
-						}
-						else if(proZip != null) {
-							queryTuples.put(QueryStrings.DEST_ZIP, proZip);
-						}	
-						return;
+					} 
+					if(retroZip != null) {
+						queryTuples.put(QueryStrings.ORIGIN_ZIP, retroZip);
 					}
+					else if(proZip != null) {
+						queryTuples.put(QueryStrings.DEST_ZIP, proZip);
+					}	
+					return;
 				}
 			}
 		}
-	}
-			
-			//if(Delivery Date > 7 Days after the EAD){
-					//Set Delivery Date = EAD + 7 Days
-					//return;}	
-			
-			//else{
-				//Set DOW = Delivery date DOW
-				//if(Destination == HFPU){
-					//if(Delivery Date is a holiday)
-					//Set DOW = 8
-					
-					//[DataAccess] Lookup Dow Close Time from [ATF_ADDRESS_CLOSE]
-					
-					//if(Close Time = 0){
-						//Set Delivery Date = Delivery Date + 1
-						//continue;
-					//}
-
-					//if Retrograde ZIP is set 
-						//Origin ZIP = Retrograde ZIP
-					
-					//else if(Prograde ZIP set)
-						//Destination ZIP = Prograde ZIP
-					  
-					//return;
-				//} 
-			
-				//else{
-					//if(DOW==Saturday or Sunday){
-						//if(User select "No Delivery" on this DOW){
-							//Delivery Date = Delivery Date + 1
-							//continue;
-						//}
-					//}
-				
-					//else if(Delivery Date is a holiday){
-						//if(User select "No Delivery" on Holidays){
-							//Set Delivery date = Delivery Date + 1
-							//continue;
-							//}
-						//else DOW = 8;
-					//}
-			
-					//if(Destination is PO Box){ 	
-						//Lookup DOW Close Time from ATF_ADDRESS_CLOSE
-						//if(Close Time = 0){
-							//Set Delivery Date = Delivery Date + 1
-							//continue;
-						//}
-
-						//if Retrograde ZIP is set 
-							//Origin ZIP = Retrograde ZIP
-						
-						//else if(Prograde ZIP set)
-							//Destination ZIP = Prograde ZIP
-						//return;
-					//}
-				//}
-			//}
 	
+	}
+
+	//if(Delivery Date > 7 Days after the EAD){
+	//Set Delivery Date = EAD + 7 Days
+	//return;}	
+
+	//else{
+	//Set DOW = Delivery date DOW
+	//if(Destination == HFPU){
+	//if(Delivery Date is a holiday)
+	//Set DOW = 8
+
+	//[DataAccess] Lookup Dow Close Time from [ATF_ADDRESS_CLOSE]
+
+	//if(Close Time = 0){
+	//Set Delivery Date = Delivery Date + 1
+	//continue;
+	//}
+
+	//if Retrograde ZIP is set 
+	//Origin ZIP = Retrograde ZIP
+
+	//else if(Prograde ZIP set)
+	//Destination ZIP = Prograde ZIP
+
+	//return;
+	//} 
+
+	//else{
+	//if(DOW==Saturday or Sunday){
+	//if(User select "No Delivery" on this DOW){
+	//Delivery Date = Delivery Date + 1
+	//continue;
+	//}
+	//}
+
+	//else if(Delivery Date is a holiday){
+	//if(User select "No Delivery" on Holidays){
+	//Set Delivery date = Delivery Date + 1
+	//continue;
+	//}
+	//else DOW = 8;
+	//}
+
+	//if(Destination is PO Box){ 	
+	//Lookup DOW Close Time from ATF_ADDRESS_CLOSE
+	//if(Close Time = 0){
+	//Set Delivery Date = Delivery Date + 1
+	//continue;
+	//}
+
+	//if Retrograde ZIP is set 
+	//Origin ZIP = Retrograde ZIP
+
+	//else if(Prograde ZIP set)
+	//Destination ZIP = Prograde ZIP
+	//return;
+	//}
+	//}
+	//}
+
 	//TODO Actually need to change to queryTuples.deliveryDate
 	public String getDeliveryDate() {
 		return droolsMsg.deliveryDate;
 	}
-	
+
 	//TODO actually implement
 	public boolean isNoDeliverySelected(int dow) {
 		return false;
 	}
-			
+
 }
